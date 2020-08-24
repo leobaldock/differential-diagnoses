54e2c3b31694f6f4a14f02137c49f3717006583d
Leo Baldock <leojdbaldock@gmail.com>
2020-08-24 10:42:27 +1000

    Setup fhir

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { getPath } from "fhirclient/lib/lib";
 
-function App() {
+const rxnorm = "http://www.nlm.nih.gov/research/umls/rxnorm";
+
+function getMedicationName(medCodings = []) {
+  let out = "Unnamed Medication(TM)";
+  const coding = medCodings.find((c) => c.system === rxnorm);
+  if (coding && coding.display) {
+    out = coding.display;
+  }
+  return out;
+}
+
+function PatientName({ name = [] }) {
+  let entry =
+    name.find((nameRecord) => nameRecord.use === "official") || name[0];
+  if (!entry) {
+    return <h1>No Name</h1>;
+  }
+  return <h1>{entry.given.join(" ") + " " + entry.family}</h1>;
+}
+
+function PatientBanner(patient) {
+  return (
+    <div>
+      <PatientName name={patient.name} />
+      <span>
+        Gender: <b>{patient.gender}</b>,{" "}
+      </span>
+      <span>
+        DOB: <b>{patient.birthDate}</b>
+      </span>
+    </div>
+  );
+}
+
+function MedRow({ med }) {
+  const name = getMedicationName(
+    getPath(med, "medicationCodeableConcept.coding") ||
+      getPath(med, "medicationReference.code.coding")
+  );
+  return (
+    <tr>
+      <td>
+        <b>{name}</b>
+      </td>
+      <td>{med.status || "-"}</td>
+      <td>{med.intent || "-"}</td>
+      <td>{getPath(med, "dosageInstruction.0.text") || "-"}</td>
+    </tr>
+  );
+}
+
+function App({ patient, meds }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PatientBanner {...patient} />
+      <hr />
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Medication</th>
+            <th>Status</th>
+            <th>Intent</th>
+            <th>Dosage Instruction</th>
+          </tr>
+        </thead>
+        <tbody>
+          {meds.map((med) => (
+            <MedRow key={med.id} med={med} />
+          ))}
+        </tbody>
+      </table>
+      {/* <pre>{ JSON.stringify(meds, null, 4) }</pre> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { oauth2 as SMART } from "fhirclient";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+
+SMART.init({
+  iss:
+    "https://launch.smarthealthit.org/v/r3/sim/eyJoIjoiMSIsImIiOiJzbWFydC0xNjQyMDY4IiwiZSI6InNtYXJ0LVByYWN0aXRpb25lci03MTYxNDUwMiJ9/fhir",
+  redirectUri: "test.html",
+  clientId: "whatever",
+  scope: "launch/patient offline_access openid fhirUser",
+})
+  .then((client) => {
+    // Fetch MedicationRequest and Patient in parallel to load the app faster
+    return Promise.all([
+      client.patient.read(),
+      client.request(`/MedicationRequest?patient=${client.patient.id}`, {
+        resolveReferences: "medicationReference",
+        pageLimit: 0,
+        flat: true,
+      }),
+    ]);
+  })
+  .then(
+    ([patient, meds]) => {
+      render(<App patient={patient} meds={meds} />, rootElement);
+    },
+    (error) => {
+      console.error(error);
+      render(
+        <>
+          <br />
+          <pre>{error.stack}</pre>
+        </>,
+        rootElement
+      );
+    }
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
