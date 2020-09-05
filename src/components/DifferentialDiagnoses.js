a37ae3046e0753a7b9d1ae5bbaf0cd936bcc96aa
James Ferreira <jameswferreira@gmail.com>
2020-09-05 16:25:03 +1000

    Added configurable buttons to TitleBar

--- src/components/DifferentialDiagnoses.js
+++ src/components/DifferentialDiagnoses.js
@@ -2,9 +2,21 @@ import React, {useState} from "react";
 import TitleBar from "./TitleBar"
 import List from "./List"
 import "./Diagnoses.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {
+    faSave,
+    faEye,
+} from '@fortawesome/free-solid-svg-icons'
+
+
 
 
 export default function DifferentialDiagnosis(){
+    const pageTitleButtons = [
+        <FontAwesomeIcon icon={faSave} size="3x" style={{cursor: "pointer"}}/>,
+        <FontAwesomeIcon icon={faEye} size="3x" style={{cursor: "pointer"}}/>,
+    ]
+
     const [rows, setRows] = useState([]);
 
     const addRow = () => {
@@ -19,7 +31,7 @@ export default function DifferentialDiagnosis(){
 
     return (
         <div>
-            <TitleBar title="Differential Diagnoses"/>
+            <TitleBar title="Differential Diagnoses" buttons={pageTitleButtons}/>
             <div class="listContainer">
                 <List title="LIKELY DIAGNOSIS" colour="#5DAD89" rows={rows} addRow={addRow} deleteRow={deleteRow}/>
             </div>
--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function TitleBar({title}){
+export default function TitleBar({title, buttons}){
 
     return (
         <div class="titleBar">
             <h1>{title}</h1>
+            
+            <div class="buttonContainer">
+                {buttons.map(x => x)}
+            </div>
         </div>
     )
 }
