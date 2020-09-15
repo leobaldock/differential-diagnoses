c8786cb7aa40a8624469109530ac9bccda0b3f20
Leo Baldock <leojdbaldock@gmail.com>
2020-09-15 21:23:32 +1000

    Remove some bad code

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,20 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import FHIR from "../state/fhir";
 
 const TitleBar = ({ title, buttons }) => {
-  const [patientState, setPatientState] = useState(null);
   const { patient } = FHIR.useContainer();
-
-  useEffect(() => {
-    setPatientState(patient);
-  }, patient);
 
   return (
     <div class="titleBar">
       <h1>
         {title.toUpperCase()}{" "}
-        {patientState &&
-          `${patientState.name[0].given[0]} ${patientState.name[0].family}`}
+        {patient && `${patient.name[0].given[0]} ${patient.name[0].family}`}
       </h1>
       <div class="buttonContainer">{buttons.map((x) => x)}</div>
     </div>
