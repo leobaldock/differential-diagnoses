c16777e137bed10d7919c6862192af52f0bbca4e
Leo Baldock <leojdbaldock@gmail.com>
2020-09-22 17:08:26 +1000

    Revert "yes"
    This reverts commit 7487323da4b9e5d5304ff6b0f4529937cb2c7152.

--- src/containers/home.js
+++ src/containers/home.js
@@ -103,13 +103,6 @@ const Home = (props) => {
       resourceType: "EpisodeOfCare",
       status: "active",
       patient: { reference: makeRef(patient) },
-      // diagnoses: [
-      //   {
-      //     condition: { reference: makeRef(condition)}
-      //     role:
-      //     rank: 1
-      //   }
-      // ]
     });
 
     console.log(episodeOfCare);
